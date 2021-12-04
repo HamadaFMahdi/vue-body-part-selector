36ad98ecda8e8a2119647274ff650acc99cb3a4d
Hamada Fadil Mahdi <hamadamahdi2014@gmail.com>
2021-12-04 23:09:46 +0000

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueBodyPartSelector from '@/vue-body-part-selector.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueBodyPartSelector
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-body-part-selector />
+  </div>
+</template>
